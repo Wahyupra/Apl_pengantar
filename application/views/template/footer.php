d9fe0492b289704dbc6e8bdc36a15769259c6b90
Wahyupra <luthfi93.lw@gmail.com>
2021-10-06 15:10:36 +0700

    initial commit

---
+++ application/views/template/footer.php
@@ -0,0 +1,7 @@
+<footer class="main-footer">
+  <div class="pull-right hidden-xs">
+    <b>Version</b> 1.2.0 (Stable)
+  </div>
+  <strong>Copyright &copy; 2020 <a href="http://Certivicate.center">Surat Pengantar RT/RW Online</a>.</strong> All rights
+  reserved.
+</footer>
